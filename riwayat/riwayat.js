3be6c22fd613080b269fbc6f0a15d964894f14bc
Arrayan <aasyamsa@gmail.com>
2023-09-08 11:47:41 +0800

    Added xml data retrievel and a little bit layout change

--- riwayat/riwayat.js
+++ riwayat/riwayat.js
@@ -1,6 +1,10 @@
+var xmlhttp = new XMLHttpRequest();
+var sumberData = "https://bmkg-content-inatews.storage.googleapis.com/last30event.xml";
+var networkDebug = true;
 const refCard = document.querySelector(".card-event");
 const cloneCard = refCard.cloneNode(true);
 refCard.remove();
+var resultxml;
 
 function ubahWarna(objek, warna) {
     objek.classList.remove("warna-biru", "warna-kuning", "warna-merah");
@@ -24,4 +28,33 @@ function tambahInfo(waktu, tanggal, kedalaman, magnitudo) {
     
     ubahWarna(newCard.querySelector(".warna-magnitudo"), magColor);
     document.querySelector(".card-list").appendChild(newCard);
+}
+
+function buatDaftar(xmlGempa) {
+    for (let i = 0; i < xmlGempa.length; i++) {
+        let waktu = xmlGempa[i].querySelector("time").innerHTML;
+        let tanggal = xmlGempa[i].querySelector("date").innerHTML;
+        let kedalaman = xmlGempa[i].querySelector("depth").innerHTML;
+        let magnitudo = xmlGempa[i].querySelector("magnitude").innerHTML;
+        tambahInfo(waktu, tanggal, kedalaman, magnitudo);
+    }
+}
+
+function statusUpdate (text) {
+    if (networkDebug) {
+        console.log(text);
+    }
+}
+
+xmlhttp.open("GET", sumberData, true);
+xmlhttp.send();
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        buatDaftar(this.responseXML.getElementsByTagName("info"))
+    } else if (this.status == 404) {
+        statusUpdate("Tidak bisa mengakses file: 404");
+    } else {
+        statusUpdate("Sedang mengupdate... " + this.readyState + "/" + this.status);
+    }
 }
