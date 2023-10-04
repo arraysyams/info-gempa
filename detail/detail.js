e45f5bc57f17a7c584f41db25aa5901dfeeba1df
Arrayan <aasyamsa@gmail.com>
2023-10-04 15:14:36 +0800

    Implementation of fetch API to process multiple XMLs

--- detail/detail.js
+++ detail/detail.js
@@ -9,37 +9,61 @@ function statusUpdate (text) {
     }
 }
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        if (this.responseXML.querySelector("info")) {
-            cachexml.push(this.responseXML.querySelector("alert"));
-        } else {
-            cachexml += this.responseXML.querySelector("Infogempa");
-        }
-    } else if (this.status == 404) {
-        statusUpdate("Tidak bisa mengakses file: 404");
-    } else {
-        statusUpdate("Sedang mengupdate... " + this.readyState + "/" + this.status);
+// xmlhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         if (this.responseXML.querySelector("info")) {
+//             cachexml.push(this.responseXML.querySelector("alert"));
+//         } else {
+//             cachexml += this.responseXML.querySelector("Infogempa");
+//         }
+//     } else if (this.status == 404) {
+//         statusUpdate("Tidak bisa mengakses file: 404");
+//     } else {
+//         statusUpdate("Sedang mengupdate... " + this.readyState + "/" + this.status);
+//     }
+// }
+
+async function getXML(url) {
+    const parser = new DOMParser();
+    let result;
+    try {
+        let response = await fetch(url);
+        result = await response.text();
+    } catch (error) {
+        result = `<error>${error}</error>`;
     }
+    return parser.parseFromString(result, "text/xml");
 }
 
 async function displayStats() {
+    // const parser = new DOMParser();
     let numsrc = urlParams.get("src");
     let eventid = urlParams.get("e");
+    let realxml;
 
     if (parseFloat(numsrc) == 1) {
-        let mag5xml, feltxml;
-        try {
-            let response = await fetch("https://bmkg-content-inatews.storage.googleapis.com/last30event.xml");
-            mag5xml = response;
-        } catch (error) {
-            mag5xml = "";
+        let mag5xml = await getXML("https://bmkg-content-inatews.storage.googleapis.com/last30event.xml");
+        let feltxml = await getXML("https://bmkg-content-inatews.storage.googleapis.com/last30feltevent.xml");
+        
+        let emag5 = mag5xml.querySelectorAll("eventid");
+        let efelt = feltxml.querySelectorAll("eventid");
+        let detailxml;
+
+        // Search information on felt first for actual update
+        for (let i = 0; i < efelt.length; i++) {
+            if (efelt[i].innerHTML == eventid) {
+                detailxml = efelt[i].parentElement;
+            }
         }
-        xmlhttp.open("GET", "https://bmkg-content-inatews.storage.googleapis.com/last30event.xml", true);
-        xmlhttp.send();
-    // } else if (parseFloat(numsrc) == 2) {
-        xmlhttp.open("GET", "https://bmkg-content-inatews.storage.googleapis.com/last30feltevent.xml", true);
-        xmlhttp.send();
+        if (typeof(detailxml) == 'undefined') {
+            for (let i = 0; i < emag5.length; i++) {
+                if (emag5[i].innerHTML == eventid) {
+                    detailxml = emag5[i].parentElement;
+                }
+            }
+        }
+        console.log(detailxml)
+
     } else if (parseFloat(numsrc) == 2) {
         xmlhttp.open("GET", "https://bmkg-content-inatews.storage.googleapis.com/live30event.xml", true);
         xmlhttp.send();
